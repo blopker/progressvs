55287a03f18f5e6a22a599016403756af56919f2
ninj0x <ninj0x@mininin.(none)>
2012-08-30 16:30:13 +0100

    cleaned up go test

--- go/test.go
+++ go/test.go
@@ -1,48 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "math"
+	"fmt"
+	"math"
+	"time"
 )
 
+type Tester func(int)
+
 func fib(n int) int {
-    if n == 0 || n == 1 {
-        return n
-    }
-    return fib(n - 1) + fib(n - 2)
+	if n == 0 || n == 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
 }
 
-func newton(N, c float64) float64{
-    x := float64(10.0)
-    error := float64(1.0)
-    for math.Abs(error) >= c {
-        error = math.Pow(x, 2) - float64(N)
-        x = x - error/(2 * x)
-    }
-    return x
+func newton(N, c float64) float64 {
+	x := 10.0
+	error := 1.0
+	for math.Abs(error) >= c {
+		error = math.Pow(x, 2) - N
+		x = x - error/(2*x)
+	}
+	return x
 }
 
 func TestFib(times int) {
-    for i := 0; i < times; i++ {
-        _ = fib(20)
-    }
+	for i := 0; i < times; i++ {
+		_ = fib(20)
+	}
 }
 
 func TestNewton(times int) {
-    for i := 0; i < times; i++ {
-        _ = newton(89262175, .0001)
-    }
+	for i := 0; i < times; i++ {
+		_ = newton(89262175, .0001)
+	}
+}
+
+func TimeIt(tester Tester, times int) time.Duration {
+	t := time.Now()
+	tester(times)
+	d := time.Since(t)
+	return d
 }
 
 func main() {
-    t := time.Now()
-    TestNewton(50000)
-    d := time.Since(t)
-    fmt.Println("Total time for Newton's method: ", d)
-    t = time.Now()
-    TestFib(100)
-    d = time.Since(t)
-    fmt.Println("Total time for Fibonacci's number: ", d)
-
+	fmt.Println("Total time for Newton's method: ", TimeIt(TestNewton, 50000))
+	fmt.Println("Total time for Fibonacci's number: ", TimeIt(TestFib, 100))
 }
